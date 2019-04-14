1a36a07300cb08e8fc347b7165d54c03db6fa64b
vcharpenay <vcharpenay@gmail.com>
2019-04-14 22:06:48 +0200

    started reformatting website, rendered with STTL instead of Jekyll

---
+++ render.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const urdf = require('urdf');
+const sttl = require('sttl');
+
+const vcharpenay = fs.readFileSync('vcharpenay.jsonld', 'utf-8');
+const bib = fs.readFileSync('bib.jsonld', 'utf-8');
+const opts = { format: 'application/ld+json' };
+
+const me = {
+    type: 'uri',
+    'value': 'https://vcharpenay.github.io/#me'
+};
+
+fs.readFileSync('templates.sparql', 'utf-8')
+.split('---')
+.forEach(tpl => sttl.register(tpl));
+
+sttl.connect(q => {
+    return urdf.query(q)
+    .then(bindings => ({
+        results: {
+            bindings: bindings
+        }
+    }));
+});
+
+urdf.load(vcharpenay, opts)
+.then(() => urdf.load(bib, opts))
+.then(() => sttl.callTemplate('https://vcharpenay.github.io/#index', me))
+.then(html => fs.writeFileSync('index.html', html));
